b2ec8edab74298a1989af65bb9a662f97cb944de
Wei Lu <luwei.here@gmail.com>
2014-02-24 11:39:16 +0800

    decode

--- index.js
+++ index.js
@@ -2,6 +2,33 @@
 var words = require('./wordlist.json')
 // list of words from http://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/Contemporary_poetry
 
+var n = words.length
+function decode(wlist) {
+  var result = ''
+  for(var i=0; i<Math.floor(wlist.length/3); i++){
+    var word1 = wlist[3*i]
+    var word2 = wlist[3*i + 1]
+    var word3 = wlist[3*i + 2]
+
+    var w1 = words.indexOf(word1)
+    var w2 = mod(words.indexOf(word2), n)
+    var w3 = mod(words.indexOf(word3), n)
+
+    var number = w1 + mod(w2-w1, n) * n + mod(w3-w2, n) * n * n
+    var hex = number.toString(16)
+    while(hex.length < 8){ hex = '0' + hex }
+    result += hex
+  }
+
+  return result
+}
+
+// always returns a non-negative number
+function mod(a, b) {
+  return (a % b + b) % b
+}
+
 module.exports = {
-  words: words
+  words: words,
+  decode: decode
 }
--- test/tests.js
+++ test/tests.js
@@ -6,3 +6,33 @@ describe('Word list', function(){
     assert.equal(1626, mnemonic.words.length);
   })
 })
+
+describe('#decode', function(){
+  it('returns empty string when there are less than 3 words', function(){
+    var wlist = [ "like", "like" ]
+    assert.equal('', mnemonic.decode(wlist))
+  })
+
+  it('works for first word repeated 3 times', function(){
+    var wlist = [ "like", "like", "like" ]
+    assert.equal('00000000', mnemonic.decode(wlist))
+  })
+
+  it('works for last word repeated 3 times', function(){
+    var wlist = [ "weary", "weary", "weary" ]
+    assert.equal('00000659', mnemonic.decode(wlist))
+  })
+
+  it('works for the largest combo', function(){
+    var wlist = [ "fail", "husband","howl" ]
+    assert.equal('ffffffff', mnemonic.decode(wlist))
+  })
+
+  it('works for last 12 words', function(){
+    var wlist = [
+      "spiral", "squeeze", "strain", "sunset", "suspend", "sympathy",
+      "thigh", "throne", "total", "unseen", "weapon", "weary"
+    ]
+    assert.equal('0028644c0028644f0028645200286455', mnemonic.decode(wlist))
+  })
+})
