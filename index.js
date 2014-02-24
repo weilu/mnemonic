1dcbdc37255b50d6d52c986c21ad3c390fe843c4
Wei Lu <luwei.here@gmail.com>
2014-02-24 13:27:11 +0800

    encode

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var words = require('./wordlist.json')
 var n = words.length
 function decode(wlist) {
   var result = ''
-  for(var i=0; i<Math.floor(wlist.length/3); i++){
+  for(var i=0; i<div(wlist.length, 3); i++){
     var word1 = wlist[3*i]
     var word2 = wlist[3*i + 1]
     var word3 = wlist[3*i + 2]
@@ -23,12 +23,33 @@ function decode(wlist) {
   return result
 }
 
+function encode(message) {
+  var result = []
+  for(var i=0; i<div(message.length, 8); i++){
+    var word = message.substr(8*i, 8)
+    var hex = parseInt(word, 16)
+
+    var w1 = mod(hex, n)
+    var w2 = mod(div(hex, n) + w1, n)
+    var w3 = mod(div(div(hex, n), n) + w2, n)
+
+    result.push(words[w1], words[w2], words[w3])
+  }
+
+  return result
+}
+
 // always returns a non-negative number
 function mod(a, b) {
   return (a % b + b) % b
 }
 
+function div(a, b) {
+  return Math.floor(a/b)
+}
+
 module.exports = {
   words: words,
-  decode: decode
+  decode: decode,
+  encode: encode
 }
--- test/tests.js
+++ test/tests.js
@@ -36,3 +36,32 @@ describe('#decode', function(){
     assert.equal('0028644c0028644f0028645200286455', mnemonic.decode(wlist))
   })
 })
+
+describe('#encode', function(){
+  it('returns empty passphrase when input is less than 8 characters', function(){
+    assert.deepEqual([], mnemonic.encode("0000000"))
+  })
+
+  it('works for first word repeated 3 times', function(){
+    var wlist = [ "like", "like", "like" ]
+    assert.deepEqual(wlist, mnemonic.encode('00000000'))
+  })
+
+  it('works for last word repeated 3 times', function(){
+    var wlist = [ "weary", "weary", "weary" ]
+    assert.deepEqual(wlist, mnemonic.encode('00000659'))
+  })
+
+  it('works for the largest combo', function(){
+    var wlist = [ "fail", "husband","howl" ]
+    assert.deepEqual(wlist, mnemonic.encode('ffffffff'))
+  })
+
+  it('works for last 12 words', function(){
+    var wlist = [
+      "spiral", "squeeze", "strain", "sunset", "suspend", "sympathy",
+      "thigh", "throne", "total", "unseen", "weapon", "weary"
+    ]
+    assert.deepEqual(wlist, mnemonic.encode('0028644c0028644f0028645200286455'))
+  })
+})
